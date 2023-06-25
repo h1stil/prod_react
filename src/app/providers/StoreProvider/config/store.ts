3a09bdde56e049611dc2af4d66fdc6e47ab71781
h1stil <jkaem111@gmail.com>
2023-06-25 11:14:11 +0300

    feat: added Counter with tests

---
+++ src/app/providers/StoreProvider/config/store.ts
@@ -0,0 +1,16 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { counterReducer } from "entities/Counter/model/slice/counterSlice";
+import { StateSchema } from "./StateSchema";
+
+export function createReduxStore(initialState?: StateSchema) {
+  return configureStore<StateSchema>({
+    reducer: {
+      counter: counterReducer,
+    },
+    devTools: __IS_DEV__,
+    preloadedState: initialState,
+  });
+}
+
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppDispatch = typeof store.dispatch;
