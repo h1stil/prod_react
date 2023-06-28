2f41f671cb795488bee9daf5244f7d7ccf4b9f0e
h1stil <jkaem111@gmail.com>
2023-06-28 18:50:49 +0300

    feat: added stories for login form and input

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,21 +1,9 @@
-import { Counter } from "entities/Counter";
-import { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Input } from "shared/ui/Input/Input";
 
 const MainPage = () => {
   const { t } = useTranslation();
-  const [value, setValue] = useState("");
-  const onChangeInput = (val: string) => {
-    setValue(val);
-  };
 
-  return (
-    <div>
-      {t("Главная страница")}
-      <Input onChange={onChangeInput} value={value} />
-    </div>
-  );
+  return <div>{t("Главная страница")}</div>;
 };
 
 export default MainPage;
